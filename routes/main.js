8405dca77d1bd5e93b4d6cc9a6ac496cd1f87f4f
imovie23 <54276075+imovie23@users.noreply.github.com>
2019-11-18 00:47:23 +0200

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.route('/')
     .get(async function (req, res) {
-        res.send('HELLO Final Project');
+        res.send('HELLO');
     });
 
 module.exports = router;
