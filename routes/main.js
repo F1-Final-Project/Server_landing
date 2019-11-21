949b17b7c1230eba7a2359ff6fe7aae0e16f4f49
Daniel <daniel0000@ukr.net>
2019-11-21 13:31:06 +0200

    layout

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const Category = require('db-schemas/lib/models/category');
-const Dish = require('db-schemas/lib/models/dish');
-const News = require('db-schemas/lib/models/news');
+const Category = require('../node_modules/db_services/lib/models/category');
+const Dish = require('../node_modules/db_services/lib/models/dish');
+const News = require('../node_modules/db_services/lib/models/news');
 
 router.route('/')
     .get(async function (req, res) {
-
         try {
             const categories = await Category.find({});
             const news = await News.find({}).sort({date: -1});
@@ -24,3 +23,4 @@ router.route('/')
             return res.status(500).send(err);
         }
     });
+module.exports = router;
